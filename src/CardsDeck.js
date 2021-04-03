7bf6901c8b7799b80bc8d06aed83b71c37685dc4
Davi Costa <daviita1@hotmail.com>
2021-04-02 22:18:06 -0300

    CardsDeck.js/index.css/index.js add

---
+++ src/CardsDeck.js
@@ -0,0 +1,21 @@
+
+const deckTable = []
+const deck = []
+const cardsHand = []
+
+
+function Hand() {
+    if (cardsHand.length < 8) deck.push(cardsHand)
+}
+function cardCreator() {
+
+}
+
+function selectWinnerCard() {
+
+}
+
+
+function cardTableSelect() {
+
+}
